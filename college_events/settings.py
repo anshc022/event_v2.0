bbac8096a2ff69e9accc7322a219b1dbd9392d33
anshc022 <rishukrchaurasia007@gmail.com>
2024-06-27 18:28:53 +0530

    uy

--- college_events/settings.py
+++ college_events/settings.py
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'college_events.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
+}
 
 
 # Password validation
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_URLS ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- college_events/wsgi.py
+++ college_events/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for CRUD project.
+WSGI config for college_events project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CRUD.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'college_events.settings')
 
 application = get_wsgi_application()
-app= application
+app = application
