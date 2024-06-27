a1148e171358eb0697cf4137f398e8106aee0304
anshc022 <rishukrchaurasia007@gmail.com>
2024-06-27 22:42:34 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
-# Install dependencies
-pip install --upgrade pip
-pip install -r requirements.txt
+# Exit immediately if a command exits with a non-zero status.
+set -e
 
-# Run Django management commands
-python manage.py migrate
-python manage.py collectstatic --noinput
+# Ensure pip is installed
+echo "Ensuring pip is installed..."
+python3.9 -m ensurepip --upgrade
+
+# Upgrade pip
+echo "Upgrading pip..."
+python3.9 -m pip install --upgrade pip
+
+# Install dependencies
+echo "Installing dependencies..."
+python3.9 -m pip install -r requirements.txt
+
+# Apply database migrations
+echo "Making Migrations..."
+python3.9 manage.py makemigrations --noinput
+
+echo "Applying Migrations..."
+python3.9 manage.py migrate --noinput
+
+# Collect static files
+echo "Collecting Static Files..."
+python3.9 manage.py collectstatic --noinput --clear
