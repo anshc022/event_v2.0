a96c6cf051ea9117fb908d39b248e9a4c13789a5
anshc022 <rishukrchaurasia007@gmail.com>
2024-06-27 20:58:08 +0530

    UU

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 echo "BUILD START"
  python3.9 -m pip install -r requirements.txt
  python3.9 manage.py collectstatic --noinput --clear
+ #!/bin/bash
+pip install -r requirements.txt
+python manage.py migrate
+python manage.py collectstatic --noinput
+
  echo "BUILD END"
