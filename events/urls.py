b6de141ebc91cd81a55099156f3a34619248baf9
anshc022 <rishukrchaurasia007@gmail.com>
2024-08-12 00:39:19 +0530

    nothink

--- events/urls.py
+++ events/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('register/<int:event_id>/', views.register, name='register'),
     path('event/<int:pk>/download-registrations/', views.download_registrations_details, name='event_download_registrations_details'),
     path('event/<int:event_id>/add-to-calendar/', views.add_to_calendar, name='add_to_calendar'),
+
     # Add other paths as needed
 ]+  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
