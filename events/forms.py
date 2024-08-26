6405be8c5618f8fb42408ea6430ff2e78cff7080
anshc022 <rishukrchaurasia007@gmail.com>
2024-08-26 17:36:05 +0530

    push

--- events/forms.py
+++ events/forms.py
@@ -16,26 +16,23 @@ class RegistrationForm(forms.ModelForm):
         team_size = kwargs.pop('team_size', 3)  # Default to 3 if not provided
         super().__init__(*args, **kwargs)
 
-        self.fields['team_name'] = forms.CharField(max_length=100, label='Team Name')
-
         # Add fields for each team member
+        self.fields['team_name'] = forms.CharField(max_length=100, label='Team Name')
         for i in range(1, team_size + 1):
             member_label = f'Member {i} Name'
             if i == 1:
                 member_label += ' (Team Leader)'
+                self.fields[f'member{i}_name'] = forms.CharField(max_length=100, label=member_label)
+                self.fields['member1_email'] = forms.EmailField(label='Member 1 Email ID')
                 self.fields['member1_mobile_number'] = forms.CharField(
                     max_length=15,
                     label='Member 1 Mobile Number',
                     required=True
                 )
-
-            self.fields[f'member{i}_name'] = forms.CharField(max_length=100, label=member_label)
-            self.fields[f'member{i}_vtu_number'] = forms.CharField(max_length=20, label=f'Member {i} VTU Number')
-            self.fields[f'member{i}_year'] = forms.IntegerField(label=f'Member {i} Year')
-
-        # Add email field only for the first member
-        if team_size > 0:
-            self.fields['member1_email'] = forms.EmailField(label='Member 1 Email ID')
+            else:
+                self.fields[f'member{i}_name'] = forms.CharField(max_length=100, label=member_label)
+                self.fields[f'member{i}_vtu_number'] = forms.CharField(max_length=20, label=f'Member {i} VTU Number')
+                self.fields[f'member{i}_year'] = forms.IntegerField(label=f'Member {i} Year')
 
     def clean_member1_email(self):
         email = self.cleaned_data.get('member1_email')
@@ -56,4 +53,11 @@ class RegistrationForm(forms.ModelForm):
 
     class Meta:
         model = Registration
-        fields = ['team_name', 'member1_email', 'member1_mobile_number']  # Updated to use 'member1_mobile_number'
+        fields = [
+            'team_name',
+            'member1_name',
+            'member1_email',
+            'member1_mobile_number'
+        ]  # Include all required fields
+
+        # Include other fields as necessary for your use case
